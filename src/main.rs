4f12435610087f3f9bccf11d2a1c3f4b831804d7
Stanislas Lange <stanislas.lange@pm.me>
2021-05-14 22:12:19 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{get, middleware, post, web, App, HttpResponse, HttpServer};
 use serde::{Deserialize, Serialize};
-use std::process::Command;
+use std::{fs, process::Command};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ExecReq {
@@ -14,12 +14,26 @@ struct ExecRes {
     stderr: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct RunCreq {
+    id: String,
+    code: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct RunCres {
+    message: String,
+    stdout: String,
+    stderr: String,
+}
+
 #[post("/exec")]
 async fn exec(req: web::Json<ExecReq>) -> HttpResponse {
     let cmd_args = req.command.split_whitespace().collect::<Vec<&str>>();
     let cmd_res = Command::new(cmd_args[0]).args(&cmd_args[1..]).output();
 
     match cmd_res {
+        Err(_err) => HttpResponse::InternalServerError().finish(),
         Ok(output) => {
             let res = ExecRes {
                 command: req.command.to_string(),
@@ -29,13 +43,46 @@ async fn exec(req: web::Json<ExecReq>) -> HttpResponse {
 
             HttpResponse::Ok().json(res)
         }
-        Err(err) => HttpResponse::InternalServerError().json(err.to_string()),
     }
 }
 
 #[get("/health")]
 async fn health() -> HttpResponse {
     HttpResponse::Ok().finish()
+}
+
+#[post("/run/c")]
+async fn run_c(req: web::Json<RunCreq>) -> HttpResponse {
+    match fs::write(format!("/tmp/{}.c", req.id), req.code.clone()) {
+        Err(_err) => HttpResponse::InternalServerError().finish(),
+        Ok(_) => {
+            let compile_res = Command::new("gcc")
+                .args(&[
+                    format!("/tmp/{}.c", req.id),
+                    "-o".to_string(),
+                    format!("/tmp/{}.out", req.id),
+                ])
+                .output();
+            match compile_res {
+                Err(_err) => HttpResponse::InternalServerError().finish(),
+                Ok(_) => {
+                    let exec_res = Command::new(format!("/tmp/{}.out", req.id)).output();
+                    match exec_res {
+                        Err(_err) => HttpResponse::InternalServerError().finish(),
+                        Ok(output) => {
+                            let res = RunCres {
+                                message: "stonks".to_string(),
+                                stdout: String::from_utf8_lossy(&output.stdout).to_string(),
+                                stderr: String::from_utf8_lossy(&output.stderr).to_string(),
+                            };
+
+                            HttpResponse::Ok().json(res)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 #[actix_web::main]
@@ -50,6 +97,7 @@ async fn main() -> std::io::Result<()> {
             .data(web::JsonConfig::default().limit(4096)) // <- limit size of the payload (global configuration)
             .service(exec)
             .service(health)
+            .service(run_c)
     })
     .bind("127.0.0.1:8080")?
     .run()
@@ -96,6 +144,31 @@ mod tests {
         assert_eq!(
             response_body,
             r##"{"command":"uname","stdout":"Linux\n","stderr":""}"##
+        );
+
+        Ok(())
+    }
+
+    #[actix_rt::test]
+    async fn test_run_c() -> Result<(), Error> {
+        let mut app = test::init_service(App::new().service(run_c)).await;
+
+        let req = test::TestRequest::post()
+            .uri("/run/c")
+            .set_json(&RunCreq { id: "123".to_string(), code: "#include <stdio.h>\r\nint main() {\r\n   printf(\"Hello, World!\");\r\n   return 0;\r\n}".to_string()})
+            .to_request();
+        let resp = app.call(req).await.unwrap();
+
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = match resp.response().body().as_ref() {
+            Some(actix_web::body::Body::Bytes(bytes)) => bytes,
+            _ => panic!("Response error"),
+        };
+
+        assert_eq!(
+            response_body,
+            r##"{"message":"stonks","stdout":"Hello, World!","stderr":""}"##
         );
 
         Ok(())
