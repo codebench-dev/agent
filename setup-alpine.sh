126793392c36ee3f421f97a50fea36dbf7d4e7ac
Stanislas Lange <stanislas.lange@pm.me>
2021-05-23 23:35:33 +0000

    fix(setup-alpine): add python2

--- setup-alpine.sh
+++ setup-alpine.sh
@@ -5,7 +5,7 @@ set -xe
 apk add --no-cache openrc
 apk add --no-cache util-linux
 apk add --no-cache gcc libc-dev
-apk add --no-cache python3
+apk add --no-cache python2 python3
 
 ln -s agetty /etc/init.d/agetty.ttyS0
 echo ttyS0 > /etc/securetty
