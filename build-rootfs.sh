d01c6e3ab0b7c2ced6ef08f5809418c85dd19fff
Stanislas Lange <stanislas.lange@pm.me>
2021-06-12 21:12:49 +0000

    fix(image): wrong golang package name

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-dd if=/dev/zero of=rootfs.ext4 bs=1M count=200
+dd if=/dev/zero of=rootfs.ext4 bs=1M count=1000
 mkfs.ext4 rootfs.ext4
 mkdir -p /tmp/my-rootfs
 mount rootfs.ext4 /tmp/my-rootfs
@@ -11,7 +11,7 @@ docker run -i --rm \
     -v /tmp/my-rootfs:/my-rootfs \
     -v "$(pwd)/target/x86_64-unknown-linux-musl/release/agent:/usr/local/bin/agent" \
     -v "$(pwd)/openrc-service.sh:/etc/init.d/agent" \
-    alpine sh < setup-alpine.sh
+    alpine sh <setup-alpine.sh
 
 umount /tmp/my-rootfs
 
